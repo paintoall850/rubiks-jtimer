c722b49e5aed2df2c7276d2dfa3fb49a2e51b10c
DougCube <DougCube@75771541-2b4d-0410-9f3d-bb97622e8ca5>
2008-05-14 11:11:11 +0000

    Adding a few files for scripting the compile on Windows cmd line. (Also executable on Linux.) The manifest.txt gives the splashScreen. There is one for compiling with the source code into the JAR. Just make sure you delete or rename all the *.JAVA and *.CLASS files first to make sure the compile is clean.

---
+++ src/makeJar.bat
@@ -0,0 +1,2 @@
+javac *.java
+jar cvmf manifest.txt RubiksJTimer.jar *.class typeOff.gif typeOn.gif Cow.gif Cow.jpg bigPicture.jpg smallPicture.jpg count.mid bing1.wav bing2.wav blip.wav ready.wav startup.wav
