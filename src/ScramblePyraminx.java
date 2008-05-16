4a170ba9b6addf40902a7024d722b2b25a04e54b
DougCube <DougCube@75771541-2b4d-0410-9f3d-bb97622e8ca5>
2008-05-16 18:20:37 +0000

    forgot to add this, it won't build without it, ops. The length 25 bug is now fixed and all tip turns occur first.

---
+++ src/ScramblePyraminx.java
@@ -0,0 +1,95 @@
+/*
+ * JNetCube
+ * Copyright (C) 2007 Chris Hunt
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ */
+
+public class ScramblePyraminx{
+    private static final String[]   U = {"U","U'"},
+                                    L = {"L","L'"},
+                                    R = {"R","R'"},
+                                    B = {"B","B'"};
+    private int previousArray, previousFace, currentArray, currentFace;
+    private String formatedMove;
+
+    public ScramblePyraminx(){
+    }
+
+    private void generateCoreMove(){
+        currentArray = (int)(Math.random()*4);
+        currentFace = (int)(Math.random()*2);
+
+        String[] arrayChoice = null;
+        switch(currentArray){
+            case 0: arrayChoice = U; break;
+            case 1: arrayChoice = L; break;
+            case 2: arrayChoice = R; break;
+            case 3: arrayChoice = B; break;
+        }
+
+        formatedMove = arrayChoice[currentFace];
+    }
+
+    public String generateScramble(){
+        String scramble = "";
+
+        int tipChoice, tipsTurned = 4;
+        tipChoice = (int)(Math.random()*3);
+        switch (tipChoice){
+            case 0: tipsTurned--; break;
+            case 1: scramble = scramble + " u"; break;
+            case 2: scramble = scramble + " u'"; break;
+        }
+        tipChoice = (int)(Math.random()*3);
+        switch (tipChoice){
+            case 0: tipsTurned--; break;
+            case 1: scramble = scramble + " l"; break;
+            case 2: scramble = scramble + " l'"; break;
+        }
+        tipChoice = (int)(Math.random()*3);
+        switch (tipChoice){
+            case 0: tipsTurned--; break;
+            case 1: scramble = scramble + " r"; break;
+            case 2: scramble = scramble + " r'"; break;
+        }
+        tipChoice = (int)(Math.random()*3);
+        switch(tipChoice){
+            case 0: tipsTurned--; break;
+            case 1: scramble = scramble + " b"; break;
+            case 2: scramble = scramble + " b'"; break;
+        }
+
+        generateCoreMove();
+        scramble = scramble + " " + formatedMove;
+        previousArray = currentArray;
+        previousFace = currentFace;
+
+        //we have one move of the scramble. Now we need to generate the rest.
+        for(int i=0; i<(24-tipsTurned); i++){
+            generateCoreMove();
+            while(previousArray == currentArray)
+                generateCoreMove();
+            scramble = scramble + " " + formatedMove;
+            previousArray = currentArray;
+            previousFace = currentFace;
+        }
+
+        if(scramble.startsWith(" ")) // truncate leading space (if there is one, but there should be)
+            scramble = scramble.substring(1, scramble.length());
+        return scramble;
+    } // end Generate Scramble
+}
